c64f051dbf9c8451773160c7cadb41e26206dae3
Antoine <antoine.elmallah@gmail.com>
2024-03-30 17:36:54 -0300

    Adding flask and marshmallow

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,27 +1,41 @@
-from entities.entity import Session, engine, Base
-from entities.exam import Exam
+from .entities.entity import Session, engine, Base
+from .entities.exam import Exam, ExamSchema
+from flask import Flask, jsonify, request
+
+# create flask application
+app = Flask(__name__)
 
 # generate database schema if needed
 Base.metadata.create_all(engine)
 
-# start session
-session = Session()
+@app.route('/exams')
+def get_exams():
 
-# check for existing data
-exams = session.query(Exam).all()
+    # fetching from database
+    session = Session()
+    exam_objects = session.query(Exam).all()
 
-if len(exams) == 0:
-    
-    # create and persist mock exam
-    python_exam = Exam('Test Exam', 'Test your knowledge', 'script')
-    session.add(python_exam)
-    session.commit()
+    # transforming into JSON-serializable objects
+    schema = ExamSchema(many=True)
+    exams = schema.dump(exam_objects)
+
+    # serializing as JSON
     session.close()
+    return jsonify(exams)
 
-    # reload exams
-    exams = session.query(Exam).all()
+@app.route('/exams', methods=['POST'])
+def add_exam():
 
-# Show existing exams
-    print('### Exams:')
-    for exam in exams:
-        print(f'({exam.id}) {exam.title} - {exam.description}')
+    # mount exam object 
+    posted_exam = ExamSchema(only=('title', 'description')).load(request.get_json())
+    exam = Exam(**posted_exam, created_by='HTTP post request')
+
+    # persist exam
+    session = Session()
+    session.add(exam)
+    session.commit()
+
+    # return created exam
+    new_exam = ExamSchema().dump(exam)
+    session.close()
+    return jsonify(new_exam), 201
